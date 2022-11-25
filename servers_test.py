ffbe6da59c035fac4cd8b321f4fbea88fdf07008
Kacper Kaczmarski <kkaczmarski.npg@gmail.com>
2022-11-25 11:26:03 +0100

    test-get_entries-produkty posortowane wg ceny

--- servers_test.py
+++ servers_test.py
@@ -1,5 +1,5 @@
 import unittest
-from collections import Counter
+import operator
  
 from servers import ListServer, Product, Client, MapServer
  
@@ -8,22 +8,12 @@ server_types = (ListServer, MapServer)
  
 class ServerTest(unittest.TestCase):
  
-    def test_get_entries_returns_proper_entries(self):
-        products = [Product('P12', 1), Product('PP234', 2), Product('PP235', 1)]
+    def test_get_entries_returns_in_ascending_order(self):
+        products = [Product('Pp12', 3), Product('PP234', 2), Product('PP235', 1)]
         for server_type in server_types:
             server = server_type(products)
             entries = server.get_entries(2)
-            self.assertEqual(Counter([products[2], products[1]]), Counter(entries))
- 
- 
-class ClientTest(unittest.TestCase):
-    def test_total_price_for_normal_execution(self):
-        products = [Product('PP234', 2), Product('PP235', 3)]
-        for server_type in server_types:
-            server = server_type(products)
-            client = Client(server)
-            self.assertEqual(5, client.get_total_price(2))
- 
+            self.assertEqual(products.sort(key=operator.attrgetter('price')), entries)
  
 if __name__ == '__main__':
     unittest.main()
