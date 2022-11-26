14b623d189b5e7291adf957b05407a6a35b1140d
Marceli Jach <marceli.jach.npg@gmail.com>
2022-11-26 23:19:49 +0100

    Test wyjątku TooManyProductsFoundError

--- servers_test.py
+++ servers_test.py
@@ -1,29 +1,16 @@
 import unittest
 from collections import Counter
  
-from servers import ListServer, Product, Client, MapServer
+from servers import ListServer, Product, Client, MapServer, TooManyProductsFoundError
  
 server_types = (ListServer, MapServer)
  
  
-class ServerTest(unittest.TestCase):
- 
-    def test_get_entries_returns_proper_entries(self):
-        products = [Product('P12', 1), Product('PP234', 2), Product('PP235', 1)]
+class ServerTest(unittest.TestCase):  
+    def TooManyProductsFoundError_throw_test(self):
+        products = [Product(f"PP{(a*2)*100+(a+3)*10+a+4}", a*7+a*2) for a in range(7)]
         for server_type in server_types:
-            server = server_type(products)
-            entries = server.get_entries(2)
-            self.assertEqual(Counter([products[2], products[1]]), Counter(entries))
- 
- 
-class ClientTest(unittest.TestCase):
-    def test_total_price_for_normal_execution(self):
-        products = [Product('PP234', 2), Product('PP235', 3)]
-        for server_type in server_types:
-            server = server_type(products)
-            client = Client(server)
-            self.assertEqual(5, client.get_total_price(2))
- 
- 
+            self.assertRaises(TooManyProductsFoundError,server_type(),self,products)
+
 if __name__ == '__main__':
     unittest.main()
