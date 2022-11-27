91a7fa0f6d78e436d56ac405bcb532c59879448c
Marceli Jach <marceli.jach.npg@gmail.com>
2022-11-27 13:47:11 +0100

    wlasciwy test TooManyProd... exception

--- servers_test.py
+++ servers_test.py
@@ -8,9 +8,11 @@ server_types = (ListServer, MapServer)
  
 class ServerTest(unittest.TestCase):  
     def TooManyProductsFoundError_throw_test(self):
-        products = [Product(f"PP{(a*2)*100+(a+3)*10+a+4}", a*7+a*2) for a in range(7)]
+        products = [Product(f"PPP{(a)*100+(a+3)*10+a+4}", a*3) for a in range(1,11)] + [Product(f"PP{(a)*100+(a+6)*10+a+1}", a*2) for a in range(1,11)]
         for server_type in server_types:
-            self.assertRaises(TooManyProductsFoundError,server_type(),self,products)
+            server = server_type(products)
+            self.assertRaises(TooManyProductsFoundError,server.get_entries(),3)
 
 if __name__ == '__main__':
     unittest.main()
+
